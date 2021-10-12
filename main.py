51aace071d2a34fe6b0f025875afcfca6a46297b
Nagi0 <75706345+Nagi0@users.noreply.github.com>
2021-10-12 19:52:35 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,55 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics.texture import Texture
-from kivy.uix.camera import Camera
 from kivy.lang import Builder
-import numpy as np
-import cv2
-from pyzbar.pyzbar import decode
+from datetime import datetime
+from playsound import playsound
 
-Builder.load_file("myapplayout.kv")
+data_atual = datetime.today()
 
 
-class AndroidCamera(Camera):
-    camera_resolution = (640, 480)
+def dates_dif(date_qr, current_date):
+    return (date_qr - current_date).days
 
-    def _camera_loaded(self, *largs):
-        self.texture = Texture.create(size=np.flip(self.camera_resolution), colorfmt='rgb')
-        self.texture_size = list(self.texture.size)
 
-    def on_tex(self, *l):
-        if self._camera._buffer is None:
-            return None
-        frame = self.frame_from_buf()
-        self.frame_to_screen(frame)
-        super(AndroidCamera, self).on_tex(*l)
+class MainApp(App):
 
-    def frame_from_buf(self):
-        w, h = self.resolution
-        frame = np.frombuffer(self._camera._buffer.tostring(), 'uint8').reshape((h + h // 2, w))
-        frame_bgr = cv2.cvtColor(frame, 93)
-        return np.rot90(frame_bgr, 3)
-
-    def frame_to_screen(self, frame):
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        for barcode in decode(frame):
-            my_data = barcode.data.decode('utf-8')
-            my_data = [my_data[0:2], my_data[3:5], my_data[6:]]
-            cv2.putText(frame_rgb, str(my_data), (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2,
-                        cv2.LINE_AA)
-        flipped = np.flip(frame_rgb, 0)
-        buf = flipped.tostring()
-        self.texture.blit_buffer(buf, colorfmt='rgb', bufferfmt='ubyte')
-
-class MyLayout(BoxLayout):
-    pass
-
-class MyApp(App):
     def build(self):
-        return MyLayout()
+        return Builder.load_string(
+"""
+#:import ZBarCam kivy_garden.zbarcam.ZBarCam
+BoxLayout:
+    orientation: 'vertical'
+    ZBarCam:
+        id: qrcodecam
+    Label:
+        size_hint: None, None
+        size: self.texture_size[0], 50
+        text: ', '.join([str(symbol.data) for symbol in qrcodecam.symbols])
+    Button:
+        on_release:
+            app.print_code(', '.join([str(symbol.data) for symbol in qrcodecam.symbols]))
+            
+"""
+)
 
-if __name__ == '__main__':
-    MyApp().run()
+    def on_start(self):
+        pass
+
+    def print_code(self, code):
+        code = code[2:12]
+        code = str(code)
+        try:
+            code = datetime.strptime(code, '%d/%m/%Y')
+            if dates_dif(code, data_atual) >= 0:
+                print('Dentro da validade')
+                playsound('Dentro da validade.mp3')
+
+            else:
+                print('Fora da validade')
+                playsound('Fora da validade.mp3')
+
+        except:
+            print('Invalido')
+            playsound('Código Inválido.mp3')
+
+
+MainApp().run()
